c711c52518be9522cc64bb0af895c32ee8c2bd5d
reinmuth66 <recra6078@gmail.com>
2026-05-04 01:37:18 +0900

    refactor: Add commitlint configuration and cz-git dependency support

--- modules/czg.nix
+++ modules/czg.nix
@@ -7,8 +7,11 @@ let
   };
 
   czg = pkgs.writeShellScriptBin "czg" ''
+    export NODE_PATH="${czgNodeModules}/node_modules"
     exec ${pkgs.nodejs}/bin/node ${czgNodeModules}/node_modules/.bin/czg "$@"
   '';
 in {
   home.packages = [ czg ];
+
+  home.file."commitlint.config.js".source = ../config/czg/commitlint.config.js;
 }
