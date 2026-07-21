4a49e9c688672fe9c5df1cefd5338fda582c0920
reinmuth66 <recra6078@gmail.com>
2026-07-21 15:06:51 +0900

    feat: Register new-theme.css and set it as default Marp theme

--- modules/marp.nix
+++ modules/marp.nix
@@ -3,6 +3,7 @@
 {
   xdg.configFile = {
     "marp/simple-theme.css".source = ../config/marp/simple-theme.css;
+    "marp/new-theme.css".source = ../config/marp/new-theme.css;
     "marp/.marprc.yml".source = ../config/marp/.marprc.yml;
   };
 
