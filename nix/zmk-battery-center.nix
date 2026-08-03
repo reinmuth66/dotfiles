d87053ef4b861a6f6be23e20fbcc5a8a7ec52d77
reinmuth66 <120767210+reinmuth66@users.noreply.github.com>
2026-08-03 09:49:18 +0000

    chore: update zmk-battery-center to v0.10.2

--- nix/zmk-battery-center.nix
+++ nix/zmk-battery-center.nix
@@ -2,11 +2,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "zmk-battery-center";
-  version = "0.10.1";
+  version = "0.10.2";
 
   src = fetchurl {
     url = "https://github.com/kot149/zmk-battery-center/releases/download/v${version}/zmk-battery-center_${version}_aarch64.app.tar.gz";
-    hash = "sha256-GkMl10RIQojNBBXlqfsnh9h/2iV3VkRZtoNf/h+afj0=";
+    hash = "sha256-ulG8+PdJbcAna1Ki6eQuXark1v2T0gMYgBauS7OxzgU=";
   };
 
   sourceRoot = ".";
