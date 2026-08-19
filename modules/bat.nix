469bf340f9a964c3954c8eef55ba26ce2deee2d8
reinmuth66 <recra6078@gmail.com>
2026-08-19 11:51:40 +0900

    feat: Add Catppuccin Macchiato theme to bat via fetchFromGitHub

--- modules/bat.nix
+++ modules/bat.nix
@@ -1,8 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.bat = {
     enable = true;
     config.theme = "Catppuccin Macchiato";
+    themes = {
+      "Catppuccin Macchiato" = {
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "bat";
+          rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
+          sha256 = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
+        };
+        file = "themes/Catppuccin Macchiato.tmTheme";
+      };
+    };
   };
 }
